e64ee2f32bfaf850f6ed73f1d8f859e1976dc219
nicolaosc <nicolaos27@gmail.com>
2021-08-02 22:23:47 +0200

    update

--- oldsite/js/mainController.js
+++
@@ -1,7 +0,0 @@
-var mainApp = angular.module('mainApp', ['ngSanitize','ui.bootstrap','ui.bootstrap.dropdown'] );
-
-mainApp.controller("mainController",function($scope,$modal) {
-
-	$scope.curriculum = curriculum;
-
-});
